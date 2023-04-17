494ab3f62804a5ee8853b9f439779da87152bfa4
Delfi <delfinamunich@gmail.com>
2023-04-17 11:35:17 -0300

    landing

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
+const greeting = "Hello, I am Delfi!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
@@ -15,7 +15,23 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-
+    <VStack>
+      <Avatar
+        size="2xl"
+        name="Delfi"
+        src="https://lh3.googleusercontent.com/a/AGNmyxZmRAcG2sRxmksY4y2vqrIJfZU75dOrv3sfUSPfvw=s96-c-rg-br100"
+      />
+      <Heading as="h1" size="md" color="white">
+        {greeting}
+      </Heading>
+      <br></br>
+      <Heading as="h3" size="lg" color="white">
+        {bio1}
+      </Heading>
+      <Heading as="h2" size="lg" color="white">
+        {bio2}
+      </Heading>
+    </VStack>
   </FullScreenSection>
 );
 
